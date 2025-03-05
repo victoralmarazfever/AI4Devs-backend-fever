47516b8b70c3c18862ab4659dcc2048e02eae051
JorgePilo <80435451+SoyJorgePilo@users.noreply.github.com>
2025-03-04 21:38:40 -0600

    Initial commit

---
+++ backend/src/presentation/controllers/candidateController.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express';
+import { addCandidate, findCandidateById } from '../../application/services/candidateService';
+
+export const addCandidateController = async (req: Request, res: Response) => {
+    try {
+        const candidateData = req.body;
+        const candidate = await addCandidate(candidateData);
+        res.status(201).json({ message: 'Candidate added successfully', data: candidate });
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            res.status(400).json({ message: 'Error adding candidate', error: error.message });
+        } else {
+            res.status(400).json({ message: 'Error adding candidate', error: 'Unknown error' });
+        }
+    }
+};
+
+export const getCandidateById = async (req: Request, res: Response) => {
+    try {
+        const id = parseInt(req.params.id);
+        if (isNaN(id)) {
+            return res.status(400).json({ error: 'Invalid ID format' });
+        }
+        const candidate = await findCandidateById(id);
+        if (!candidate) {
+            return res.status(404).json({ error: 'Candidate not found' });
+        }
+        res.json(candidate);
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+export { addCandidate };
